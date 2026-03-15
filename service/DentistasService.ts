79c7b73e1413f3d7226451735981a38007783c45
NatanSantana <natanmoreira075@gmail.com>
2026-03-15 20:29:08 -0300

    verificar se já existe uma data marcada para o mesmo dentista

--- service/DentistasService.ts
+++ service/DentistasService.ts
@@ -19,6 +19,12 @@ async function registrarDentista(data: any) {
 async function lancarDatas(data: any) {
     const horarioMaximo = parse("17:00", "HH:mm", new Date())
     const horarioMinimo = parse("08:00", "HH:mm", new Date())
+    
+    if (!isValid(data.horario)) {
+        throw new Error(data.horario + " Este formato de horário não é permitido")
+
+    }
+
     const horaConsulta = parse(data.horario, "HH:mm", new Date())
     data.data = addHours(data.data, horaConsulta.getHours())
     data.data = addMinutes(data.data, horaConsulta.getMinutes());
@@ -29,15 +35,13 @@ async function lancarDatas(data: any) {
         throw new Error("Não é possível marcar uma data que já passou")
     }
 
-    const horarioRecebido = parse(datas.horario, "HH:mm", new Date())
-
-    if (!isValid(horarioRecebido)) {
-        throw new Error(horarioRecebido + " Este formato de horário não é permitido")
-
+    if (isAfter(horaConsulta, horarioMaximo) || isBefore(horaConsulta, horarioMinimo)) {
+        throw new Error("Um horário só pode ser registrado se for DEPOIS das 08:00 e ANTES das 17:00")
     }
 
-    if (isAfter(horarioRecebido, horarioMaximo) || isBefore(horarioRecebido, horarioMinimo)) {
-        throw new Error("Um horário só pode ser registrado se for DEPOIS das 08:00 e ANTES das 17:00")
+    const isConsultaExists = await DatasDisponiveis.findOne({ dentista: data.dentista, data: data.data, horario: data.horario })
+    if (isConsultaExists) {
+        throw new Error("Já existe uma consulta para esse dentista nessa mesma data e hora")
     }
 
     const dentistaBuscado = await Dentistas.findOne({ _id: datas.dentista  });
